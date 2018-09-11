104e99ba95ee65d8dc2410a7444ce4e110a0bbe1
Nirmal1067 <nirmal1067@gmail.com>
2018-09-11 11:45:53 +0530

    Adding Main Logic

--- Kiran/src/MultiLinearRegression.java
+++ Kiran/src/MultiLinearRegression.java
@@ -9,8 +9,45 @@ public class MultiLinearRegression
 	
 	private Double yintercept;
 	
+	private double learningRate=0.2;
 	
-	private Double derivativeofCoefficents(Double[] derivatives, int index,int derivativeIndex)
+	private int epoch;
+	
+	
+	private void batchGradientDescent()
+	{
+		for(int i=0;i<epoch;i++)
+		{
+			double[] derivatives = new double[slopes.length];
+			int length = traingSet.get(0).size();
+			double derivativesIntercept =0;
+			for(int j=0;j<length;j++)
+			{
+				for(int z=0;z<slopes.length;z++)
+				{
+					
+				
+				derivatives[z] = derivatives[z]+ derivativeofCoefficents(i,z);
+			
+				}
+				
+				derivativesIntercept = derivativeOfIntercept(i);	
+				
+		     }
+			 
+		yintercept = yintercept - learningRate * derivativesIntercept;
+		
+		for(int k=0;k<slopes.length;k++)
+		{
+			slopes[k] = slopes[k] - learningRate*derivatives[k];
+		}
+		
+				
+		}
+	}
+	
+	
+	private Double derivativeofCoefficents(int index,int derivativeIndex)
 	{
 		
 		Double x = -(traingSet.get(derivativeIndex).get(index));
@@ -25,6 +62,18 @@ public class MultiLinearRegression
 		derivative = derivative + yintercept;
 		
 		return derivative;
+	}
+	
+	private Double derivativeOfIntercept(int index)
+	{
+		Double gradient = traingSet.get(0).get(index);
+		
+		for(int i=0 ; i <slopes.length;i++)
+		{
+			gradient = gradient + slopes[i]*traingSet.get(i).get(index);
+		}
+		
+		return gradient;
 	}
 	
 }
